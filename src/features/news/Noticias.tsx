48d9fae18b52f8884bc7efb15c66a44fcf2d740e
Mateo_Chauchet <mateo.chauchet@mercadolibre.com>
2022-07-11 20:07:06 -0300

    fix

--- src/features/news/Noticias.tsx
+++ src/features/news/Noticias.tsx
@@ -39,32 +39,8 @@ const Noticias = () => {
 
   useEffect(() => {
     const obtenerInformacion = async () => {
+      
       const respuesta = await obtenerNoticias();
-
-      // const data = respuesta.map((n) => {
-      //   const titulo = n.titulo
-      //     .split(" ")
-      //     .map((str) => {
-      //       return str.charAt(0).toUpperCase() + str.slice(1);
-      //     })
-      //     .join(" ");
-
-      //   const ahora = new Date();
-      //   const minutosTranscurridos = Math.floor(
-      //     (ahora.getTime() - n.fecha.getTime()) / 60000
-      //   );
-
-      //   return {
-      //     id: n.id,
-      //     titulo,
-      //     descripcion: n.descripcion,
-      //     fecha: `Hace ${minutosTranscurridos} minutos`,
-      //     esPremium: n.esPremium,
-      //     imagen: n.imagen,
-      //     descripcionCorta: n.descripcion.substring(0, 100),
-      //   };
-      // });
-
       const data = normalizer(respuesta)
       setNoticias(data);
     };
